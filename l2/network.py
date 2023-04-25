1ff70e2bde80fb45df45afbab74b0bec9b4429b1
Spiryd <neumann.maks@gmail.com>
2023-04-25 22:17:49 +0200

    final l2

--- l2/network.py
+++ l2/network.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
 import random
 from pick import pick
 from copy import deepcopy
+import pandas as pd
+import seaborn as sns
+
+PING_STANDARD_SIZE = 256
 
 
 def flow(graph, matrix):
@@ -27,11 +30,11 @@ def capacity(graph):
     """Generates capacity and assigns it to a Graph
 
     Args:
-        graph (networkx Graph): Graph to assign capacity to
+        graph (networkx Graph): Graph to assign capacity to 
     """
     nx.set_edge_attributes(graph, 0, "capacity")
     for i, j in graph.edges:
-        graph[i][j]["capacity"] = graph[i][j]["flow"]  // 5 * 50 + 50
+        graph[i][j]["capacity"] = ((graph[i][j]["flow"] * 10) + PING_STANDARD_SIZE)
 
 def T(graph, matrix_sum, m):
     """Calculates avg. latency of a packet in a given Network
@@ -45,13 +48,16 @@ def T(graph, matrix_sum, m):
         Float: avg. latency of a packet
     """
     T = 0
+    #print(matrix_sum)
     for i, j in graph.edges:
         a = graph[i][j]["flow"]
         c = graph[i][j]["capacity"]
         if a >= c / m:
+            #print("None")
             return None
         else:
             T += a / (c / m - a)
+    #print(T / matrix_sum)
     return T / matrix_sum
 
 
@@ -83,9 +89,12 @@ def reliability(graph, matrix, T_max, p, m, iterations=100, intervals=10):
                     break
                 flow(trial_graph, matrix)
                 t = T(trial_graph, matrix_sum, m)
+                #print(t)
             else:
                 t = base_t
+                #print(t)
             if not t or t >= T_max:
+                #print("wrong")
                 break
             successful_trials += 1
     return successful_trials / (iterations * intervals)
@@ -152,7 +161,7 @@ def experiment2(graph, matrix, T_max, p, m, iterations=10):
     results = [reliability(test_graph, matrix, T_max, p, m)]
     for _ in range(iterations):
         for i, j in test_graph.edges:
-            test_graph[i][j]["capacity"] += 50
+            test_graph[i][j]["capacity"] += PING_STANDARD_SIZE
         results.append(reliability(test_graph, matrix, T_max, p, m))
     return results
 
@@ -203,67 +212,63 @@ def gen_intensity_matrix():
 
 def t1(G, N):
     suma = sum(sum(r) for r in N)
-    min_Tmax = [T(G, suma, m) for m in range(1, 11)]
-    for m in range(4, 0, -2):
-        startT = min_Tmax[m - 1]
-        for p in [0.9, 0.95, 0.99]:
-            plt.figure()
-            plt.imshow(
-                [
-                    experiment1(G, N, Tmax, p, m, step=100)
-                    for Tmax in np.linspace(startT, 10 * startT, num=10)
-                ],
-                extent=[0, 1000, startT, 10 * startT],
-                aspect="auto",
-                origin="lower",
-                cmap=mpl.colormaps["plasma"]
-            )
-            plt.colorbar()
+    M = [1, 2, 3, 4]
+    P = [0.90, 0.95, 0.99]
+    TMAX = [T(G, suma, i) for i in M]
+    for p in P:
+        for m in M:
+            res = []
+            for Tmax in TMAX:
+                for i in list(enumerate(experiment1(G, N, Tmax, p, m, step=(PING_STANDARD_SIZE*25), iterations=20))):
+                    res.append([Tmax, i[0], i[1]])
+            df = pd.DataFrame(res, columns=['Tmax', 'iter', 'res'])
+            df = df.pivot("Tmax", "iter", "res")
+            sns.heatmap(df, cmap='plasma')
             plt.ylabel("T_max")
-            plt.xlabel("Liczba dodanych pakietów przy p={}, m={}".format(p, m))
-            plt.savefig("TEST1_{}_{}.png".format(m, p))
-            plt.close()
+            plt.xlabel(f"Iteracja dodawania pakietów przy p={p}, m={m}")
+            plt.savefig(f"TEST1_{p}_{m}.png")
+            plt.clf()
+    
+
 
 def t2(G, N):
     suma = sum(sum(r) for r in N)
-    min_Tmax = [T(G, suma, m) for m in range(1, 11)]
-    for m in range(4, 0, -2):
-        startT = min_Tmax[m - 1]
-        for p in [0.9, 0.95, 0.99]:
-            plt.figure()
-            plt.imshow(
-                [experiment2(G, N, Tmax, p, m) for Tmax in np.linspace(startT, 10 * startT, num=10)],
-                extent=[0, 10, startT, 10 * startT],
-                aspect="auto",
-                origin="lower",
-                cmap=mpl.colormaps["plasma"]
-            )
-            plt.colorbar()
+    M = [1, 2, 3, 4]
+    P = [0.90, 0.95, 0.99]
+    TMAX = [T(G, suma, i) for i in M]
+    for p in P:
+        for m in M:
+            res = []
+            for Tmax in TMAX:
+                for i in list(enumerate(experiment2(G, N, Tmax, p, m, iterations=20))):
+                    res.append([Tmax, i[0], i[1]])
+            df = pd.DataFrame(res, columns=['Tmax', 'iter', 'res'])
+            df = df.pivot("Tmax", "iter", "res")
+            sns.heatmap(df, cmap='plasma')
             plt.ylabel("T_max")
-            plt.xlabel("Wzrost przepustowości każdego łącza w b/s p={}, m={}".format(p, m))
-            plt.savefig("TEST2_{}_{}.png".format(m, p))
-            plt.close()
+            plt.xlabel(f"Iteracja wzroztu przepustowosci przy p={p}, m={m}")
+            plt.savefig(f"TEST2_{p}_{m}.png")
+            plt.clf()
 
 def t3(G, N):
     suma = sum(sum(r) for r in N)
-    min_Tmax = [T(G, suma, m) for m in range(1, 11)]
-
-    for m in range(4, 0, -1):
-        startT = min_Tmax[m - 1]
-        for p in np.linspace(0.9, 0.99, num=10):
-            plt.figure()
-            plt.imshow(
-                [experiment3(G, N, Tmax, p, m) for Tmax in np.linspace(startT, 10 * startT, num=10)],
-                extent=[0, 10, startT, 10 * startT],
-                aspect="auto",
-                origin="lower",
-                            cmap=mpl.colormaps["plasma"]
-            )
-            plt.colorbar()
+    M = [1, 2, 3, 4]
+    P = [0.90, 0.95, 0.99]
+    TMAX = [T(G, suma, i) for i in M]
+    for p in P:
+        for m in M:
+            res = []
+            for Tmax in TMAX:
+                for i in list(enumerate(experiment3(G, N, Tmax, p, m, iterations=20))):
+                    res.append([Tmax, i[0], i[1]])
+            df = pd.DataFrame(res, columns=['Tmax', 'iter', 'res'])
+            df = df.pivot("Tmax", "iter", "res")
+            sns.heatmap(df, cmap='plasma')
             plt.ylabel("T_max")
-            plt.xlabel("Liczba dodanych krawędzi przy p={}, m={}".format(p, m))
-            plt.savefig("TEST3_{}_{}.png".format(m, p))
-            plt.close()
+            plt.xlabel(f"Iteracja dodawania krawedzi przy p={p}, m={m}")
+            plt.savefig(f"TEST3_{p}_{m}.png")
+            plt.clf()
+
 
 def see_graph(G):
     """Generates a visual repesentation of the network
@@ -294,7 +299,30 @@ def main():
     G.add_edge(7, 12)
     G.add_edge(12, 17)
     print(G)
-    N = [[0, 2, 8, 4, 7, 8, 2, 7, 3, 4, 7, 6, 1, 7, 1, 5, 3, 6, 2, 7], [7, 0, 6, 6, 2, 3, 0, 4, 3, 0, 7, 3, 8, 6, 5, 3, 0, 2, 1, 6], [0, 8, 0, 7, 8, 1, 6, 8, 7, 2, 1, 8, 7, 3, 0, 0, 6, 1, 4, 4], [2, 3, 8, 0, 6, 5, 7, 8, 2, 3, 5, 4, 7, 1, 8, 8, 8, 3, 3, 7], [7, 1, 6, 5, 0, 5, 6, 0, 5, 8, 3, 6, 2, 0, 2, 3, 2, 2, 1, 4], [0, 5, 6, 3, 6, 0, 3, 0, 2, 6, 0, 3, 1, 3, 3, 7, 6, 0, 0, 5], [2, 3, 4, 8, 6, 4, 0, 0, 7, 4, 2, 4, 0, 5, 5, 2, 7, 4, 2, 4], [4, 7, 3, 4, 5, 6, 2, 0, 4, 1, 0, 0, 3, 5, 1, 4, 1, 5, 5, 4], [8, 8, 0, 3, 7, 2, 7, 0, 0, 6, 1, 7, 0, 1, 4, 4, 3, 1, 4, 3], [7, 4, 0, 4, 1, 7, 2, 6, 5, 0, 0, 3, 2, 5, 5, 8, 1, 6, 6, 5], [0, 4, 6, 6, 6, 0, 2, 7, 8, 5, 0, 2, 1, 1, 1, 5, 0, 6, 6, 2], [2, 5, 1, 4, 8, 0, 6, 7, 0, 1, 3, 0, 3, 6, 0, 3, 3, 4, 2, 6], [1, 5, 3, 4, 0, 2, 4, 4, 4, 3, 5, 0, 0, 7, 4, 6, 1, 3, 5, 0], [4, 1, 3, 1, 6, 6, 8, 3, 3, 4, 5, 1, 5, 0, 2, 7, 7, 2, 0, 2], [7, 6, 3, 8, 1, 2, 5, 2, 3, 1, 2, 5, 5, 7, 0, 1, 4, 1, 0, 1], [7, 8, 1, 0, 0, 6, 8, 7, 0, 7, 7, 0, 4, 3, 6, 0, 3, 4, 7, 3], [3, 7, 2, 4, 3, 3, 4, 6, 7, 3, 0, 5, 3, 1, 8, 2, 0, 4, 6, 6], [5, 4, 5, 8, 3, 5, 7, 1, 7, 1, 7, 2, 2, 3, 7, 7, 2, 0, 1, 0], [1, 1, 7, 7, 2, 5, 0, 7, 3, 5, 7, 4, 4, 6, 5, 7, 8, 1, 0, 6], [1, 6, 0, 5, 6, 8, 8, 2, 5, 7, 2, 8, 1, 7, 3, 0, 0, 5, 2, 0]]
+    
+    N = [
+        [0, 2, 8, 4, 7, 8, 2, 7, 3, 4, 7, 6, 1, 7, 1, 5, 3, 6, 2, 7], 
+        [7, 0, 6, 6, 2, 3, 0, 4, 3, 0, 7, 3, 8, 6, 5, 3, 0, 2, 1, 6], 
+        [0, 8, 0, 7, 8, 1, 6, 8, 7, 2, 1, 8, 7, 3, 0, 0, 6, 1, 4, 4], 
+        [2, 3, 8, 0, 6, 5, 7, 8, 2, 3, 5, 4, 7, 1, 8, 8, 8, 3, 3, 7], 
+        [7, 1, 6, 5, 0, 5, 6, 0, 5, 8, 3, 6, 2, 0, 2, 3, 2, 2, 1, 4], 
+        [0, 5, 6, 3, 6, 0, 3, 0, 2, 6, 0, 3, 1, 3, 3, 7, 6, 0, 0, 5], 
+        [2, 3, 4, 8, 6, 4, 0, 0, 7, 4, 2, 4, 0, 5, 5, 2, 7, 4, 2, 4], 
+        [4, 7, 3, 4, 5, 6, 2, 0, 4, 1, 0, 0, 3, 5, 1, 4, 1, 5, 5, 4], 
+        [8, 8, 0, 3, 7, 2, 7, 0, 0, 6, 1, 7, 0, 1, 4, 4, 3, 1, 4, 3], 
+        [7, 4, 0, 4, 1, 7, 2, 6, 5, 0, 0, 3, 2, 5, 5, 8, 1, 6, 6, 5], 
+        [0, 4, 6, 6, 6, 0, 2, 7, 8, 5, 0, 2, 1, 1, 1, 5, 0, 6, 6, 2], 
+        [2, 5, 1, 4, 8, 0, 6, 7, 0, 1, 3, 0, 3, 6, 0, 3, 3, 4, 2, 6], 
+        [1, 5, 3, 4, 0, 2, 4, 4, 4, 3, 5, 0, 0, 7, 4, 6, 1, 3, 5, 0], 
+        [4, 1, 3, 1, 6, 6, 8, 3, 3, 4, 5, 1, 5, 0, 2, 7, 7, 2, 0, 2], 
+        [7, 6, 3, 8, 1, 2, 5, 2, 3, 1, 2, 5, 5, 7, 0, 1, 4, 1, 0, 1], 
+        [7, 8, 1, 0, 0, 6, 8, 7, 0, 7, 7, 0, 4, 3, 6, 0, 3, 4, 7, 3], 
+        [3, 7, 2, 4, 3, 3, 4, 6, 7, 3, 0, 5, 3, 1, 8, 2, 0, 4, 6, 6], 
+        [5, 4, 5, 8, 3, 5, 7, 1, 7, 1, 7, 2, 2, 3, 7, 7, 2, 0, 1, 0], 
+        [1, 1, 7, 7, 2, 5, 0, 7, 3, 5, 7, 4, 4, 6, 5, 7, 8, 1, 0, 6], 
+        [1, 6, 0, 5, 6, 8, 8, 2, 5, 7, 2, 8, 1, 7, 3, 0, 0, 5, 2, 0]
+        ]
+    N = [[j*PING_STANDARD_SIZE for j in i] for i in N]
     flow(G, N)
     capacity(G)
     title = "Choose option: "
